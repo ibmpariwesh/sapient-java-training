11662a3b232703dee7623428c9d0a2c40e86723e
pariwesh gupta (C) <pariwesh.gupta@gmail.com>
2022-04-07 09:33:10 +0530

    abstract classes

--- day-2/src/InnerClasses.java
+++ day-2/src/InnerClasses.java
@@ -9,19 +9,11 @@ public class InnerClasses {
 
 class Outer {
 	int a = 1;
-
 	class Inner {
 		int a = 2;
-
 		void method1() {
 			System.out.println(a);
 			System.out.println(new Outer().a);
 		}
-	}
-}
-
-class Engine {
-	class Piston {
-
 	}
 }
