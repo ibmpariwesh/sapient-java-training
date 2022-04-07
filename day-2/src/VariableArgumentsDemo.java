beb44917a4f8143703af4e881334a899ea2d850b
pariwesh gupta (C) <pariwesh.gupta@gmail.com>
2022-04-07 09:11:10 +0530

    variable arguments

---
+++ day-2/src/VariableArgumentsDemo.java
@@ -0,0 +1,24 @@
+
+public class VariableArgumentsDemo {
+	public static void main(String[] args) {
+		int[] numbers = new int[] {1,2,3,4,5,6};
+		int result = new VariableArgumentsDemo().sum(numbers);
+		System.out.println(result);
+	}
+
+	private int sum(int... numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			total = total + number;
+		}
+		return total;
+	}
+	
+	private int sum1(int[] numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			total = total + number;
+		}
+		return total;
+	}
+}
