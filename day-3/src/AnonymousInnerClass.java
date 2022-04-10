6df3f2c81421ae698620da3478fc5b5248ba44b0
pariwesh gupta (C) <pariwesh.gupta@gmail.com>
2022-04-10 21:37:30 +0530

    day-3 code

---
+++ day-3/src/AnonymousInnerClass.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class AnonymousInnerClass {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add(1);
+		list.add(-2);
+		list.add(-8);
+		System.out.println(list);
+		list.sort(new Comparator() {
+
+			@Override
+			public int compare(Object number1, Object number2) {
+				return ((Integer)number1 - (Integer)number2);
+			}
+		});
+		System.out.println(list);
+	}
+
+}
